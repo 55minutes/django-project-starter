5a25c702df59269369fb203f6f144799b4d904a3
George Song <george@55minutes.com>
2013-05-29 14:09:35 -0700

    Add VIRTUAL_ENV constant

--- deploy/__init__.py
+++ deploy/__init__.py
@@ -1,4 +1,7 @@
 from os.path import abspath, dirname, join
+import sys
+
 
 PACKAGE_ROOT = dirname(dirname(abspath(__file__)))
 PROJECT_ROOT = join(PACKAGE_ROOT, '{{ project_name }}')
+VIRTUAL_ENV = abspath(sys.prefix)
