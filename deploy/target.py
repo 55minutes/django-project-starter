b0c2cc978e957a1a966ffb5ed0afc0f88ab79499
George Song <george@55minutes.com>
2013-06-08 11:22:54 -0700

    Set the db_user

--- deploy/target.py
+++ deploy/target.py
@@ -1,3 +1,4 @@
+from os import environ
 from os.path import abspath, dirname, join
 import sys
 
@@ -21,4 +22,5 @@ def local():
     env.settings_fp = join(env.project_root, 'settings.py')
     env.static_root = join(env.project_root, 'assets')
     env.secret_key = get_random_string(54)
+    env.db_user = environ['USER']
     env.db_password = ''
